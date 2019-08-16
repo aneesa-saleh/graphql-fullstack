807e5702f904fc7795c2432829cf53b96b76fb87
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-08-16 02:54:07 +0100

    Add resolvers for launches, launch and me queries

---
+++ server/src/resolvers.js
@@ -0,0 +1,11 @@
+// fieldName: (parent, args, context, info) => data;
+
+module.exports = {
+  Query: {
+    launches: (_, __, { dataSources }) =>
+      dataSources.launchAPI.getAllLaunches(),
+    launch: (_, { id }, { dataSources }) =>
+      dataSources.launchAPI.getLaunchById({ launchId: id }),
+    me: (_,__, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+  }
+}
