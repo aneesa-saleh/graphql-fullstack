09147e86ed4217ed813a88eab1ef1d5ca11f8e2d
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-08-16 04:27:52 +0100

    Add resolvers for mission, launch and user types

--- server/src/schema.js
+++ server/src/schema.js
@@ -46,7 +46,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
