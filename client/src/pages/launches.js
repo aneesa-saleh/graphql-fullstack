cd40870353bd02e33d94e70bc6bfb89805833bc2
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-08-20 21:44:17 +0100

    Add a button to enable pagination by loading more launches

--- client/src/pages/launches.js
+++ client/src/pages/launches.js
@@ -26,7 +26,7 @@ const GET_LAUNCHES = gql`
 `;
 
 export default function Launches() {
-  const { data, loading, error } = useQuery(GET_LAUNCHES);
+  const { data, loading, error, fetchMore } = useQuery(GET_LAUNCHES);
   if (loading) return <Loading />;
   if (error) return <p>ERROR</p>;
 
@@ -38,6 +38,29 @@ export default function Launches() {
           <LaunchTile key={launch.id} launch={launch} />
         ))
       }
+      {data.launches && data.launches.hasMore && (
+        <Button onClick={() => fetchMore({
+          variables: {
+            after: data.launches.cursor,
+          },
+          updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+            if (!fetchMoreResult) return prev;
+            
+            return {
+              ...fetchMoreResult,
+              launches: {
+                ...fetchMoreResult.launches,
+                launches: [
+                   ...prev.launches.launches,
+                   ...fetchMoreResult.launches.launches
+                ]
+              }
+            };
+          }
+        })}>
+          Load More
+        </Button>
+      )}
     </Fragment>
   );
 }
